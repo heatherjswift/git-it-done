3aef17a754f415817f9e7ba125935ba80c477a0e
Hede <Heatherj.swift@gmail.com>
2021-03-25 10:48:02 -0400

    convert fetched data into dom elements

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -7,9 +7,16 @@ var getUserRepos = function(user) {
 
     //make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data, user);
         });
+    } else {
+        alert("Error: " + response.statusText);
+    }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to GitHub");
     });
 };
 
@@ -32,6 +39,11 @@ var formSubmitHandler = function(event) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     //clear old content
