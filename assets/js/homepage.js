1d1f4cca588f2b4a9a7ae4473abf9bd4b94d0e1c
Hede <Heatherj.swift@gmail.com>
2021-03-26 12:35:16 -0400

    redirect errors loading or incorrect repo names back to homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,25 @@
+var userFormEl = document.querySelector ("#user-form");
+var nameInputEl = document.querySelector ("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+        nameInputEl.value ="";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -20,24 +40,6 @@ var getUserRepos = function(user) {
     });
 };
 
-var userFormEl = document.querySelector ("#user-form");
-var nameInputEl = document.querySelector ("#username");
-
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-
-    //get value from input element
-    var username = nameInputEl.value.trim();
-
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value ="";
-    } else {
-        alert("Please enter a GitHub username");
-    }
-    console.log(event);
-};
-
 var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
     if (repos.length === 0) {
@@ -47,7 +49,7 @@ var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
     //clear old content
-    repoContainerEl.textContent = "";
+    
     repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
@@ -56,8 +58,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         //create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         //create a span element to hold repository name
         var titleEl = document.createElement("span");
